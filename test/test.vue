c219dcc6d9219879ed085c3d68fc96d0569bd90e
yepengjie <609596602@qq.com>
2020-04-02 16:52:13 +0800

    Site updated: 2020-04-02 16:52:12

---
+++ test/test.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-form :model="trance" :rules="rules">
+        <el-form-item prop="data">
+            <el-input
+                v-model="data"
+                onkeypress="return(/[0-9]/.test(String.fromCharCode(event.keyCode)))"
+            ></el-input>
+        </el-form-item>
+    </el-form>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            trance: {
+                data: ""
+            },
+            rules: {
+                data: {
+                    validator(rule, value, callback) {
+                        if (
+                            Number.isInteger(Number(value)) &&
+                            Number(value) >= 0 &&
+                            Number(value) <= 3000
+                        ) {
+                            callback();
+                        } else {
+                            callback(new Error("最大数值范围3000"));
+                        }
+                    },
+                    trigger: 'change'
+                }
+            }
+        };
+    }
+};
+</script>
